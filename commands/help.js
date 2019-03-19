0e6821613a092d03b27fe682ddb51c9d0378e384
xlnd47 <axel.deschuijmer@gmail.com>
2019-03-19 19:47:08 +0100

    push

---
+++ commands/help.js
@@ -0,0 +1,18 @@
+const prefix = process.env.PREFIX
+
+module.exports = {
+    name : "help",
+    description : "List all of my commands or info about a specific command.",
+    aliases : ["commands"],
+    usage : ["command name"],
+    cooldown : 5,
+    execute(message, args){
+
+        const data = [];
+        const { commands } = message.client;
+
+        if (!args.length) {
+            // ...
+        }
+    }
+}
