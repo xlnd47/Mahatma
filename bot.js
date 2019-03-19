0abaae908773d51bf558e490093c72929b56cd9e
xlnd47 <axel.deschuijmer@gmail.com>
2019-03-19 18:53:22 +0100

    command handler

--- bot.js
+++ bot.js
@@ -1,8 +1,18 @@
+const fs = require("fs");
 const Discord = require("discord.js"); // We Call The Packages.
 // const PREFIX = "<"; // You can change this Prefix to whatever you want.
 const PREFIX = process.env.PREFIX;
 
 var bot = new Discord.Client();
+
+bot.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles){
+    const command = require(`./commands/${file}`);
+    bot.commands.set(command.name, command);
+}
 
 // Events.
 bot.on("ready", function() {
@@ -12,80 +22,21 @@ bot.on("ready", function() {
 
 bot.on("message", function(message) {
 
-    if (message.author.bot) return;
-
-    if (!message.guild) return;
-
-    if (!message.content.startsWith(PREFIX)) return;
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
     
     var args = message.content.substring(PREFIX.length).split(" ");
     var command = args[0].toLowerCase();
 
-// Commands.
-    if (command == "help") {
-        var embedhelpmember = new Discord.RichEmbed()
-            .setAuthor("💬 Command List.")
-            .addField(" - avatar", "Show your Avatar.")
-            .addField(" - ping", "PING PONG.")
-            .setColor(0x00FFEE)
-            .setFooter("Ⓒ 2019 Mahatma Bot.", bot.user.displayAvatarURL);
-        var embedhelpadmin = new Discord.RichEmbed()
-            .setAuthor("💬 Moderator Commands.")
-            .addField(" - prune", "Prune up to `99` Messages.")
-            .addField(" - kick", "Kick someone from your Server.")
-            .setColor(0x00FFEE)
-            .setFooter("Ⓒ 2019 Mahatma Bot.", bot.user.displayAvatarURL);
-            message.channel.send(embedhelpmember)
-        if(message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send(embedhelpadmin);
-    };
+    if (!bot.commands.has(command)) return;
 
-    if (command == "avatar") {
-        let member = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-        message.channel.send({
-               embed: {
-                  title: `${member.displayAvatarURL}'s Profile Picture.`,
-                  image: {
-                      url: member.AvatarURL
-                  },
-                  color: 0x00FFEE
-               }
-        })
-    };
-
-    if (command == "ping") {
-        message.channel.send("**:ping_pong: PONG!**");
-    };
-
-    if(command === "prune") {
-        if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply("**🔒 Sorry, you can't do that.**");
-        var messagesToDelete = args[1];
-        if (!args[1]) return message.channel.send("❌ Please include the amount of Message that you want to **Prune**!");
-        if (args[1] > 99) return message.channel.send("❌ I can't **Prune** more than `99` Messages.");
-        message.channel.fetchMessages({limit: messagesToDelete})
-        .then(messages => message.channel.bulkDelete(messages.size + 1))
-        .catch(error => message.channel.send(`❌ Sorry ${message.author}, Failed while **Prunning** because: *${error}*.`));
-    };
-
-    if(command == "kick") {
-        message.delete()
-        let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-        if(!kUser) return message.channel.send("❌ Please **@mention** your target!");
-        let kReason = args.join(" ").slice(0);
-        if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("**🔒 Sorry, you can't do that.**");
-        if(kUser.hasPermission("KICK_MEMBERS")) return message.channel.send("❌ Failed to **Kick**, need a higher than Roles.");
+    try {
+        bot.commands.get(command).execute(message, args);
+    } catch (error){
+        console.error(error);
+        message.reply("There was an error trying to execute that command!");
+    }
     
-        let kickEmbed = new Discord.RichEmbed()
-        .setDescription("**👢 Kicked**")
-        .setColor(0xFF0000)
-        .addField("User", `${kUser}`)
-        .addField("Moderator", `<@${message.author.id}>`)
-        .addField("Reason", `**\`\`\`${kReason}\`\`\`**`);
     
-        let adminlog = message.guild.channels.find(`name`, "mod-logs");
-        if(!adminlog) return message.channel.send("❌ Sorry, i need the Logging Channels with name **#mod-logs**.");
-        message.guild.member(kUser).kick(kReason);
-        adminlog.send(kickEmbed);
-    };
 
 });
 
