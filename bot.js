9aae435f45ef281b10dc9775ff4ba6c3f005e4cf
xlnd47 <axel.deschuijmer@gmail.com>
2019-03-19 19:06:35 +0100

    commands and packages

--- bot.js
+++ bot.js
@@ -1,45 +1,38 @@
 const fs = require("fs");
 const Discord = require("discord.js"); // We Call The Packages.
 // const PREFIX = "<"; // You can change this Prefix to whatever you want.
-const PREFIX = process.env.PREFIX;
+const prefix = process.env.PREFIX;
 
-var bot = new Discord.Client();
-
-bot.commands = new Discord.Collection();
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-for (const file of commandFiles){
-    const command = require(`./commands/${file}`);
-    bot.commands.set(command.name, command);
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
 }
 
-// Events.
-bot.on("ready", function() {
-    bot.user.setGame(`Ghandi`);
-    console.log(`${bot.user.username} is Ready!`);
+client.once('ready', () => {
+	console.log('Ready!');
 });
 
-bot.on("message", function(message) {
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
-    
-    var args = message.content.substring(PREFIX.length).split(" ");
-    var command = args[0].toLowerCase();
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
 
-    if (!bot.commands.has(command)) return;
+	if (!client.commands.has(command)) return;
 
-    try {
-        bot.commands.get(command).execute(message, args);
-    } catch (error){
-        console.error(error);
-        message.reply("There was an error trying to execute that command!");
-    }
-    
-    
-
+	try {
+		client.commands.get(command).execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
 });
 
 // Bot Login.
 // bot.login('YourAwesomeBotToken');
-bot.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);
