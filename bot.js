bd92310b5e8195167aa495a03e832185a6ef625f
xlnd47 <axel.deschuijmer@gmail.com>
2019-03-19 18:06:10 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ var bot = new Discord.Client();
 
 // Events.
 bot.on("ready", function() {
-    bot.user.setGame(`Hi, Im New Bot On Discord!`);
+    bot.user.setGame(`Hi, Im Mahatma!`);
     console.log(`${bot.user.username} is Ready!`);
 });
 
